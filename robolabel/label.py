7e1fb7c58d6398d4c38a0d561f351862b38cf626
chris98122 <chris98122@126.com>
2019-01-29 17:15:06 +0800

    mainpage finished annotation improved

--- robolabel/label.py
+++ robolabel/label.py
@@ -38,52 +38,54 @@ def get_post(id, check_author=True):
        print("post",id," is none")
        
     return post
-@lb.route('/getNewImage', methods=('GET', 'POST'))
 
+
+@lb.route('/getNewImage', methods=('GET', 'POST'))
+@login_required
 def getNewImage():
     root_dir = os.path.abspath(os.path.dirname(__file__))
     img_path=root_dir+'\static'+'\images'
-    #post = get_post(id+1) 
     files = os.listdir(img_path)
     
-    former_id =request.form.get('id')
-    former_id=int(former_id)
-    former_id+=1
-    print(former_id)
-    file= "/static/images/"+files[former_id]
-    print(file)
+    id =request.form.get('id')
+    id=int(id)
+    if(id==len(files)):
+        id=len(files)
+    
+    file= "/static/images/"+files[id]
     return jsonify(url=file)
 
 
+    
+@lb.route('/getPost', methods=('GET', 'POST'))
+@login_required
+def getPost():
+    id =request.form.get('id')
+    title=request.form.get('title')
+    id=int(id)
+    post = get_post(id+1) 
+    return jsonify(label=post['title'])
+
 
 #from flask import send_from_directory
-@lb.route('/<int:id>/mainpage', methods=('GET', 'POST'))
+@lb.route('/mainpage', methods=('GET', 'POST'))
 @login_required
-def mainpage(id):
+def mainpage():
     root_dir = os.path.abspath(os.path.dirname(__file__))
     img_path=root_dir+'\static'+'\images'
-    post = get_post(id+1) 
+    
     files = os.listdir(img_path)
-     #####显示图片
-     ##显示方式： n-1到n页（显示为第0张）往后是第1张
-    if id==len(files):
-        id=0
-        file= "/static/images/"+files[id]
-        post = get_post(id+1) 
-        
-    else:
-        file= "/static/images/"+files[id]
-
-    #####   显示图片 结束
+    file= "/static/images/"+files[0]
     ##### 上传分类的表单   
- 
-    if request.method == 'POST':
-        
-        title = request.form['title']
+    post = get_post(1) 
 
+    if request.method == 'POST':
+        id= request.form['id']
+        id=int(id)
+        title = request.form['title']
+        post = get_post(id+1) 
         body = files[id]
         error = None
-        
         if not title:
             error = 'Category is required.'
 
@@ -100,9 +102,9 @@ def mainpage(id):
             )
             db.commit()
             if id==len(files)-1:
-                return redirect( url_for('label.mainpage', id=0) )
+                return redirect( url_for('label.mainpage') )
             else:
-               return redirect( url_for('label.mainpage', id=id+1) )
+               return redirect( url_for('label.mainpage') )
         ##### 如果db里面有post 则update    
         elif post is not None:
             db = get_db()
@@ -113,43 +115,39 @@ def mainpage(id):
             db.commit()
             
             if id==len(files)-1:
-                return redirect( url_for('label.mainpage', id=0) )
+                return redirect( url_for('label.mainpage') )
             else:
-                return redirect( url_for('label.mainpage', id=id+1) )
+                return redirect( url_for('label.mainpage') )
         
-    return render_template('label/mainpage.html',file=file,id=id,post=post)
+    return render_template('label/mainpage.html',file=file,post=post)
     
 
 
-@lb.route('/<int:id>/annotation', methods=('GET', 'POST'))
+@lb.route('/annotation', methods=('GET', 'POST'))
 @login_required
-def annotation(id):
-   
+def annotation():
+    
     root_dir = os.path.abspath(os.path.dirname(__file__))
     img_path=root_dir+'\static'+'\images'
     files = os.listdir(img_path)
-     #####显示图片
-     ##显示方式： n-1到n页（显示为第0张）往后是第1张
-    if id==len(files):
-        id=0
-        file= "/static/images/"+files[id]
+    id =0
+    id=int(id)
+    if(id==len(files)):
+        id=len(files)
     
-    else:
-        file= "/static/images/"+files[id]
-
+    file= "/static/images/"+files[id]
     if request.method == 'POST':
-        title = request.form['title']
-        x=request.form['x']
-        body = files[id]
-        error = None
-        
-        if not title:
-            error = 'tag is required.'
-
-        if error is not None: 
-            flash(error)
-        print("x:",x)
-        print("title:",title)
+        id =0
+        id=int(id)
+        if(id==len(files)):
+            id=len(files)
+    
+        file= "/static/images/"+files[id]
+        print(file)
+        folder=""
+        annotations=''
+        return jsonify(url=file,id=id,\
+        folder=folder,annotations=annotations)
         
     
     return render_template('label/annotation.html',file=file,id=id)
